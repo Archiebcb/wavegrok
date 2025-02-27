6f27963c1b0967d186e76eee42786e858a122c28
archie <archiecope@mac.home>
2025-02-27 21:47:57 +0000

    Add no-cache headers

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return Response(render_template('index.html'), mimetype='text/html')
+    resp = Response(render_template('index.html'), mimetype='text/html')
+    resp.headers['Content-Type'] = 'text/html; charset=utf-8'
+    resp.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    return resp
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=False)
-
